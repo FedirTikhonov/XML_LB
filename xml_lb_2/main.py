f9ef95c9a7c7ba6dbbb2e8224396f6df6a54d5d8
FedirTikhonov <fedir.tikhonov@gmail.com>
2022-12-02 21:22:12 +0100

    task_five fixed

--- xml_lb_2/main.py
+++ xml_lb_2/main.py
@@ -7,4 +7,4 @@ import task_one
 import random
 
 if __name__ == '__main__':
-    task_one.main()
+    task_five.main()
--- xml_lb_2/task_five.py
+++ xml_lb_2/task_five.py
@@ -4,7 +4,19 @@ import xml.dom.minidom
 def main():
     tree = xml.dom.minidom.parse("deliveries.xml")
     articles = tree.getElementsByTagName("article")
+    supplierDict = {}
+    supplierNames = []
     for article in articles:
         name = article.getElementsByTagName("name")[0]
         supplier = article.getElementsByTagName("supplier")[0]
-        print(f"{supplier.firstChild.data} supplies: {name.firstChild.data}")
+        if supplier.firstChild.data not in supplierDict:
+            supplierDict[supplier.firstChild.data] = []
+            supplierNames.append(supplier.firstChild.data)
+            supplierDict[supplier.firstChild.data].append(name.firstChild.data)
+        else:
+            supplierDict[supplier.firstChild.data].append(name.firstChild.data)
+    for supplier in supplierNames:
+        print(f"{supplier} supplies: ", end=' ')
+        for product_id in range(len(supplierDict[supplier])):
+            print(supplierDict[supplier][product_id], end=' ')
+        print()
